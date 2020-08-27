40621c847bc4b9a2b310fa218091bbc8eb12b34f
jtb324 <james.baker@vanderbilt.edu>
2020-08-26 22:44:29 -0500

    finished the script so that task are added to the app and then updated the readme.

--- README.md
+++ README.md
@@ -8,6 +8,12 @@ The purpose of this project is to create a python script that interacts with the
 
 - **Read_calendar**: This is the script with the CLI interface code and the class Calendar_xlsx for reading in the calendar document and then getting interacting with the api.
 
+  - _all useful flags_:
+    -c: This is the calendar page
+    -k: This is the users api page
+    -flp: This flag will generate a json of all of the user's projects
+    -ct: This flag will create a new task for each due date in the passed calendar
+
 ## Current Progress:
 
-- At the moment, all the script does is load the provided xlsx sheet into a pandas dataframe and then use the api key to get all projects if the user passed the correct argument.
+- The CLI has the ability to list all current projects when True is passed to teh -flp flag and the CLI can add all the due dates from the calendar if the -ct is set to true. Currently is only set for the specific project that I had in mind. It uses the id number to identify the project which would be hard for the user to specify if they don't know it off the top of their head
--- Read_calendar.py
+++ Read_calendar.py
@@ -2,6 +2,7 @@ import pandas as pd
 import numpy as np
 from todoist.api import TodoistAPI
 import argparse
+import datetime
 
 # import calendar into pandas df
 
@@ -12,9 +13,14 @@ class Calendar_xlxs:
         self.calendar = pd.read_excel(
             calendar_path, header=None, skiprows=4, usecols=[0, 1, 2, 3, 4], names=["Month", "Date", "Weekday", "Topic", "Reading Assignment"])
 
+        self.apikey = apikey
+
         print(self.calendar)
 
-        self.apikey = apikey
+        self.project_name = 2241251289
+        self.cur_year = datetime.datetime.now().year
+        self.cur_month = datetime.datetime.now().strftime("%B")
+        self.cur_day = datetime.datetime.now().day
 
     def get_all_projects(self):
 
@@ -23,6 +29,98 @@ class Calendar_xlxs:
         api.sync()
 
         print(api.state["projects"])
+
+    def create_task(self):
+        print("Creating the new task ...")
+
+        api = TodoistAPI(self.apikey)
+
+        api.sync()
+
+        # get a starting month
+        for row in self.calendar.itertuples():
+            # 0 index = tuple index
+            # 1 index = Month
+            # 2 index = Date
+            # 3 index = Weekday
+            # 4 index = Topic
+            # 5 index = Reading Assignment
+            month = row[1]
+
+            try:
+                prior_month
+
+            except NameError:
+                prior_month = None
+
+            try:
+
+                not_num = float(row[1])
+                is_nan = np.isnan(not_num)
+
+            except ValueError:
+
+                is_nan = False
+
+            if is_nan or row[1] == " ":
+                month = prior_month
+
+            prior_month = month
+
+            # get the date
+            date = row[2]
+
+            try:
+
+                not_num_date = float(row[2])
+                is_nan_date = np.isnan(not_num_date)
+
+            except ValueError:
+
+                is_nan_date = False
+
+            if is_nan_date:
+                continue
+
+            if month == self.cur_month and self.cur_day > date:
+                continue
+
+            # get the topic
+            topic = row[4]
+
+            if topic == " ":
+                continue
+
+            # get the reading pages
+            pages = row[5]
+
+            try:
+
+                not_num_page = float(row[5])
+                is_nan_page = np.isnan(not_num_page)
+
+            except ValueError:
+
+                is_nan_page = False
+
+            if not is_nan_page:
+
+                full_topic = " ".join(["HGEN 1 Class", topic, pages])
+
+            else:
+                full_topic = topic
+
+            due_date = " ".join([str(date), str(month), str(self.cur_year)])
+            print(due_date)
+            item = api.items.add(full_topic,
+                                 project_id=self.project_name,
+                                 priority=3,
+                                 due={"string": due_date}
+                                 )
+
+            api.commit()
+####################################################################################################
+# This is the CLI interface
 
 
 def run(args):
@@ -33,10 +131,14 @@ def run(args):
 
         calendar_handler.get_all_projects()
 
+    if args.new_task:
+
+        calendar_handler.create_task()
+
 
 def main():
     parser = argparse.ArgumentParser(
-        description="")
+        description="This is a command line to add task to the todoist app")
 
     parser.add_argument("-c", "--calendar", help="This argument takes the path to the calendar excel sheet",
                         dest="cal", type=str, required=True)
@@ -46,6 +148,9 @@ def main():
 
     parser.add_argument("-flp", help="This argument will print a full list of projects if set to true",
                         dest="flp", type=bool, required=False, default=False)
+
+    parser.add_argument("-ct", "--new_task", help="This argument will create a new task",
+                        dest="new_task", type=bool, required=False, default=False)
 
     parser.set_defaults(func=run)
     args = parser.parse_args()
